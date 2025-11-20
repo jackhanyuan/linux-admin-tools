1e05b1dc948f7f11cd9b853fa1e144ff652bd327
三水 <jackhanyuan@foxmail.com>
2025-11-20 16:41:11 +0800

    Comment out sources.list backup and update commands
    Comment out commands for backing up and updating sources.list

--- autorun5.0/pre.sh
+++ autorun5.0/pre.sh
@@ -9,9 +9,9 @@ echo '外网使用情况如下：'
 if ping -c4 www.baidu.com &>/dev/null
 then
     echo '已通网'
-	sudo cp  /etc/apt/sources.list /etc/apt/sources.list.bak
-	sudo cp  $PATH1/$shver/sources.list /etc/apt/
-	sudo sed -i "s/bionic/`lsb_release -c --short`/" /etc/apt/sources.list
+#	sudo cp  /etc/apt/sources.list /etc/apt/sources.list.bak
+#	sudo cp  $PATH1/$shver/sources.list /etc/apt/
+#	sudo sed -i "s/bionic/`lsb_release -c --short`/" /etc/apt/sources.list
 	sudo apt update -y 
 	sudo apt install vim ssh dkms dialog htop stress memtester lm-sensors ipmitool -y 
 	sudo apt upgrade -y 
